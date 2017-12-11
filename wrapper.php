57142983a69e33c253c2ba42c34fb418f8b8b3de
福田康弘 <vac201@gmail.com>
2017-12-11 11:15:23 +0900

    コーディングエラー対応 https://travis-ci.org/1yaan/wp-bitcoin-chart/jobs/314553311 gitignore修正

--- wrapper.php
+++ wrapper.php
@@ -54,7 +54,7 @@ function register_jquery() {
 	wp_enqueue_script( 'momentjs' );
 
 	wp_register_script( 'chartjs', 'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js', array(), '0.1.0' );
-  wp_enqueue_script( 'chartjs' );
+	wp_enqueue_script( 'chartjs' );
 }
 
 add_action( 'wp_enqueue_scripts', 'register_jquery' );
